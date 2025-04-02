3bced1da618b5eef24d1331b13013457ceb3020b
Diogo Ferreira <diogoveigaferreira@hotmail.com>
2025-04-01 23:40:01 -0300

    feat: adding cors to app

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,10 @@ const pessoas = require('./RoutePessoa');
 const cursos = require('./RouteCurso');
 const matriculas = require('./RouteMatricula');
 const categorias = require('./RouteCategoria');
-
+const cors = require('cors');
 module.exports = app =>{
   app.use(
+    cors(),
     express.json(),
     pessoas,
     cursos,
